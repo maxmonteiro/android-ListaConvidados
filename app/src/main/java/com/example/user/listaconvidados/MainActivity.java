a4633ecf8e9ce0fb244bde04c374a87c5e17173b
Maximo <maxmonteiro86@gmail.com>
2017-12-28 23:10:54 -0300

    Navegando entre activities

--- app/src/main/java/com/example/user/listaconvidados/MainActivity.java
+++ app/src/main/java/com/example/user/listaconvidados/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.user.listaconvidados;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.user.listaconvidados.R.id.button_confirm;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = findViewById(R.id.text_today);
+        this.mViewHolder.textDtEvent = findViewById(R.id.text_dt_evento);
+        this.mViewHolder.buttonConfirm = findViewById(button_confirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_confirm) {
+            // Lógica de navegação
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder {
+        TextView textToday;
+        TextView textDtEvent;
+        Button buttonConfirm;
     }
 }
---
+++ app/src/main/java/com/example/user/listaconvidados/util/SecurityPreferences.java
@@ -0,0 +1,8 @@
+package com.example.user.listaconvidados.util;
+
+/**
+ * Created by Maximo on 28/12/2017.
+ */
+
+public class SecurityPreferences {
+}
